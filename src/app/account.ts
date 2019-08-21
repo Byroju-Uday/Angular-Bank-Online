bc1821a1b8f5c79ae4c954faeef186fd59254c95
Nidhi-Kanala <sreenidhi.kanala@gmail.com>
2019-08-21 14:29:11 +0530

    Account Functionality

---
+++ src/app/account.ts
@@ -0,0 +1,14 @@
+export class Account{
+    
+    constructor()
+    {
+   
+    }
+        accountId:number;
+        accountType:string;
+        accountNumber:number;
+        accountBalance:number;
+        customerId:number;
+    
+    
+   }
