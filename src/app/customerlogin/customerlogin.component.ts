df5ceccb43be6086a5b1befe9cae91a2b6a5b7ca
Nidhi-Kanala <sreenidhi.kanala@gmail.com>
2019-08-29 15:58:32 +0530

    Customer Login Validation

--- src/app/customerlogin/customerlogin.component.ts
+++ src/app/customerlogin/customerlogin.component.ts
@@ -5,7 +5,7 @@ import { CustomerService } from '../customer.service';
 import { stringify } from '@angular/core/src/util';
 import { AuthService } from '../auth.service';
 import { formArrayNameProvider } from '@angular/forms/src/directives/reactive_directives/form_group_name';
-
+import { Customer } from '../Customer';
 
 @Component({
   selector: 'app-customerlogin',
@@ -14,55 +14,71 @@ import { formArrayNameProvider } from '@angular/forms/src/directives/reactive_di
 })
 export class CustomerloginComponent implements OnInit {
 
+  customers: Customer[];
+
   constructor(
-    private router:Router,private customerService:CustomerService, public authService: AuthService) { }
-  login1:boolean;
+    private router: Router, private customerService: CustomerService, public authService: AuthService) { }
+  login1: boolean;
   ngOnInit() {
 
-    if(localStorage.getItem("isLoggedIn")=="true" )
-    {
-      if(localStorage.getItem("isCustomer")=="true"){
+    if (localStorage.getItem("isLoggedIn") == "true") {
+      if (localStorage.getItem("isCustomer") == "true") {
         this.router.navigate(['/']);
       }
-      else{
+      else {
         this.router.navigate(['/']);
       }
-      
+
     }
+    this.customerService.listCustomers().subscribe(customers =>
+      this.customers = customers);
   }
-  
-  login(data:CustomerCredentials)
-  {
+
+  login(data: CustomerCredentials) {
     let customerId = data.customerId;
     let password = data.password;
     console.log(`this is from login funtion`);
+    if (!this.verifyCustomerExists(customerId)) {
+      document.getElementById("myModal").style.display = "block";
+      return;
+    }
     this.customerService.validateCustomerLoginCredentials(data).subscribe(response => {
       console.log('Response from login function of customerlogin.component.ts');
       console.log(response);
-      if(response===true){
+      if (response === true) {
         console.log("coming into true condition");
-       // this.authService.addCustomer(customerId,password);
-       console.log("this.authService.logInOrOut is "+this.authService.logInOrOut);
-       this.authService.LogTheCustomerOrBanker(data.customerId+"","true");
-      //  this.authService.setLogin(true);
-      //  this.authService.timering(1000);
-      //  console.log("this.authService.logInOrOut is "+this.authService.logInOrOut);
-      //   localStorage.setItem('isLoggedIn', "true");
-      //   localStorage.setItem('token', data.customerId+"");
-      //   localStorage.setItem('isCustomer',"true");
-        this.customerService.login=true;
-        this.login1=true;
-        this.router.navigate(['/customer',customerId]);
+        // this.authService.addCustomer(customerId,password);
+        console.log("this.authService.logInOrOut is " + this.authService.logInOrOut);
+        this.authService.LogTheCustomerOrBanker(data.customerId + "", "true");
+        //  this.authService.setLogin(true);
+        //  this.authService.timering(1000);
+        //  console.log("this.authService.logInOrOut is "+this.authService.logInOrOut);
+        //   localStorage.setItem('isLoggedIn', "true");
+        //   localStorage.setItem('token', data.customerId+"");
+        //   localStorage.setItem('isCustomer',"true");
+        this.customerService.login = true;
+        this.login1 = true;
+        this.router.navigate(['/customer', customerId]);
       }
-      else
-      {
-        document.getElementById("myModal").style.display="block"; 
+      else {
+        document.getElementById("myModal").style.display = "block";
       }
     });
   }
 
   functionclose(form) {
-    document.getElementById("myModal").style.display="none";  
+    document.getElementById("myModal").style.display = "none";
     form.reset();
   }
+
+  verifyCustomerExists(customerId) {
+    let i: number;
+    let customerExists = false;
+    for (i = 0; i < this.customers.length; i++) {
+      if (this.customers[i].customerId == customerId)
+        customerExists = true;
+    }
+    return customerExists;
+  }
+
 }
